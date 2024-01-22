77d01680ed5f69fd2c72403c935dc0d4dc317979
Leonardo Soares <leonardoxp9d@gmail.com>
2024-01-22 15:44:20 -0300

    refactor: add sponsors name to logo hover effect

--- src/components/Reasons/Reasons.jsx
+++ src/components/Reasons/Reasons.jsx
@@ -4,14 +4,9 @@ import image1 from "../../assets/image1.png";
 import image2 from "../../assets/image2.png";
 import image3 from "../../assets/image3.png";
 import image4 from "../../assets/image4.png";
-import nb from "../../assets/nb.png";
-import adidas from "../../assets/adidas.png";
-import nike from "../../assets/nike.png";
 import tick from "../../assets/tick.png";
-
 import { SiJordan, SiNike,SiPuma } from "react-icons/si";
 import { CgAdidas } from "react-icons/cg";
-
 
 const Reasons = () => {
   return (
@@ -63,14 +58,26 @@ const Reasons = () => {
         </span>
 
         <div className="partners">
-          {/*
-          <img src={nb} alt="" />
-          <img src={adidas} alt="" />
-          <img src={nike} alt="" />*/}
-          <SiJordan />
-          <CgAdidas/>
-          <SiNike/>
-          <SiPuma/>
+          <div className="partner">
+            <SiJordan className="partner-logo"/>
+            <span className="partner-name">Jordan</span>
+          </div>
+
+          <div className="partner">
+            <CgAdidas className="partner-logo"/>
+            <span className="partner-name">Adidas</span>
+          </div>
+
+          <div className="partner">
+            <SiNike className="partner-logo"/>
+            <span className="partner-name">Nike</span>
+          </div>
+          
+          <div className="partner">
+            <SiPuma className="partner-logo"/>
+            <span className="partner-name">Puma</span>
+          </div>
+          
         </div>
 
       </div>
